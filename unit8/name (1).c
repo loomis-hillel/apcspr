71cd3446369139dbb11bce1b47463e845da518d3
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2023-01-24 19:52:36 -0500

    Add files via upload

---
+++ unit8/name (1).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+/**
+ * name.c
+ *
+ * Accepts the user's name as input and prints
+ * the characters in the name one line at a time.
+ *
+ * @author Ken Loomis
+ */
+int main(void)
+{
+	int count, length;
+    string name = get_string( "Enter your name: " );
+    length = strlen ( name );
+    for(count = 0; count < length; count++)
+    {
+    	printf ( "%c\n", name[count] );
+    }
+
+}
