cfc7402458ff339646f30c00e73149a01e359f58
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2023-01-31 19:43:57 -0500

    Add files via upload

---
+++ unit8/decToBit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+/**
+ * This converts a number from a positive decimal (whole number)
+ * To a binary number (bitstring). 
+ * @author Ken Loomis 
+ */
+int main(void)
+{	
+	int value = get_int( "Enter a whole number: " );
+    int bitVal;
+    // Create a string that has room for 32 characters (+ the terminating
+    // character).
+	char bitstring [33];
+    // Fill the array with 0s
+    for ( int i =0; i<32; i++ ){
+        bitstring[i] = '0';
+    } 
+    // Put a the terminating character at the end of the array.
+    bitstring[32] = '\0';
+    // As long as there are still bits needed to make up the
+    // bitstring, keep dividing. Built the bitstring from
+    // the right to the left.
+    int pos = 31;
+    while ( value > 0 ) {
+        bitVal = value % 2;
+        bitstring[pos] = bitVal+'0';
+        pos--;
+        value = value / 2;
+    }
+    // Print it when you are done.
+    printf ( "%s\n", bitstring );
+	return 0;
+}
